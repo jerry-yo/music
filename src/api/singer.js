1b2b14bd696cbf5a86a416e6fb392af319de9fc4
yangjing <yangjingpuyu@aliyun.com>
2018-05-11 01:24:29 +0800

    music-list

--- src/api/singer.js
+++ src/api/singer.js
@@ -18,3 +18,21 @@ export function getSingerList () {
 
   return jsonp(url, data, options)
 }
+
+export function getSingerDetail (singerId) {
+  const url = 'https://c.y.qq.com/v8/fcg-bin/fcg_v8_singer_track_cp.fcg'
+
+  const data = Object.assign({}, commonParams, {
+    hostUin: 0,
+    needNewCode: 0,
+    platform: 'yqq',
+    order: 'listen',
+    begin: 0,
+    songstatus: 1,
+    g_tk: 5381,
+    singermid: singerId,
+    num: 100
+  })
+
+  return jsonp(url, data, options)
+}
--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,14 +1,65 @@
 <template>
   <transition name="slide">
-    <div class="singer-detail">
-
-    </div>
+    <musicList :title="title" :bg-image="bgImage" :songs="songs"></musicList>
   </transition>
 </template>
 
 <script type="text/ecmascript-6">
-export default {
-}
+  import {mapGetters} from 'vuex'
+  import {getSingerDetail} from '@/api/singer'
+  import {ERR_OK} from '@/api/config'
+  import {createSong} from '@/common/js/song'
+  import musicList from '@/components/music-list/music-list'
+
+  export default {
+    data () {
+      return {
+        songs: []
+      }
+    },
+    computed: {
+      title () {
+        return this.singer.name
+      },
+      bgImage () {
+        return this.singer.avatar
+      },
+      ...mapGetters([
+        'singer'
+      ])
+    },
+    created () {
+      console.log(this.singer)
+      this._getSingerDetail()
+    },
+    methods: {
+      _getSingerDetail () {
+        if (!this.singer.id) {
+          this.$router.push('/singer')
+          return
+        }
+        getSingerDetail(this.singer.id).then((res) => {
+          if (res.code === ERR_OK) {
+            this.songs = this._normalizeSongs(res.data.list)
+            console.log(this.songs)
+          }
+        })
+      },
+      _normalizeSongs (list) {
+        let ret = []
+        list.forEach((item) => {
+          let {musicData} = item
+          if (musicData.songid && musicData.albummid) {
+            ret.push(createSong(musicData))
+          }
+        })
+        return ret
+      }
+    },
+    components: {
+      musicList
+    }
+  }
 </script>
 
 <style lang="stylus" scoped ref="stylesheet/stylus">
