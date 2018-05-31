d7aa291c4a2637c5fe4037e92de5f26d4cfaa084
yangjing <yangjingpuyu@aliyun.com>
2018-05-31 23:40:38 +0800

    search

---
+++ src/api/search.js
@@ -0,0 +1,37 @@
+import jsonp from 'common/js/jsonp'
+import {commonParams, options} from './config'
+
+export function getHotKey () {
+  let url = 'https://c.y.qq.com/splcloud/fcgi-bin/gethotkey.fcg'
+
+  const data = Object.assign({}, commonParams, {
+    platform: 'h5',
+    needNewCode: 1
+  })
+
+  return jsonp(url, data, options)
+}
+
+export function searchAll (query, page, zhida) {
+  let url = 'https://c.y.qq.com/soso/fcgi-bin/search_for_qq_cp'
+
+  const data = Object.assign({}, commonParams, {
+    w: query,
+    catZhida: zhida ? 1 : 0,
+    p: page,
+    platform: 'h5',
+    needNewCode: 1,
+    zhidaqu: 1,
+    t: 0,
+    flag: 1,
+    ie: 'utf-8',
+    sem: 1,
+    aggr: 0,
+    perpage: 20,
+    n: 20,
+    g_tk: 5381,
+    remoteplace: 'txt.mqq.all'
+  })
+
+  return jsonp(url, data, options)
+}
